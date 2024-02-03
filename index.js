07ebb75daec46475280114a4be259848584c8a12
Akbarali404 <akbarsobirov226@gmail.com>
2024-02-03 16:13:36 +0500

    people

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const data = require('./data.json');
 
 const app = express();
 const cors = require('cors');
 app.use(cors()); 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(data);
 });
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log('Example app listening on port 3000!');
+  console.log(`Example app listening on port ${port}!`);
 });
